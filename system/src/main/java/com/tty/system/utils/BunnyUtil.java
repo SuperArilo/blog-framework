b1a536a7bdda8eee31d8d89e9bf292b5e5fc538e
SuperArilo <728152568@qq.com>
2025-05-13 17:12:48 +0800

    update

--- system/src/main/java/com/tty/system/utils/BunnyUtil.java
+++ system/src/main/java/com/tty/system/utils/BunnyUtil.java
@@ -1,6 +1,5 @@
 package com.tty.system.utils;
 
-import com.tty.common.enums.TypeContentEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
